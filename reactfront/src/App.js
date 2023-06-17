93aa1c28fc21d48c2743b36def5cef226b12e130
Carl0sss <rafailan99@gmail.com>
2023-06-16 23:39:39 -0600

    Initial Project configurations

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -1,18 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import SideBar from './components/SideBar';
 
 function App() {
   return (
-
-    <div className="App">
+      <div className="App">
       <NavBar />
       <SideBar />
-      <header className="App-header">
-        <h1>SISCOT</h1>
-      </header>
-    </div>
+      </div>
   );
 }
 
