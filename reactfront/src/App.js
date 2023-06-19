08e00658882b874538348f4cd260ecd1740a44f1
Kevin Rogel <91396196+KevinR31@users.noreply.github.com>
2023-06-18 21:23:30 -0600

    Parte 1
    Incluye un crud principal

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -1,14 +1,59 @@
 import './App.css';
-import NavBar from './components/NavBar';
-import SideBar from './components/SideBar';
+import Navbar from './componentes/Navbar'
+import Sidebar from './componentes/Sidebar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ShowMateriasPrimas from './componentes/ShowMateriasPrimas';
+import CreateMateriasPrimas from './componentes/CreateMateriasPrimas';
+import EditMateriasPrimas from './componentes/EditMateriasPrimas';
+
 
 function App() {
   return (
-      <div className="App">
-      <NavBar />
-      <SideBar />
+    <div className="container">
+      {/* Contenido principal */}
+      <div className="content">
+        {/* Header */}
+          <div>
+            {/* Espacio para el logo del proyecto */}
+            {/* Menú de búsqueda */}
+            <form align="center">
+              <div className="app">
+                <Navbar />
+                {/* Resto del contenido de la aplicación */}
+              </div>
+            </form>
+          </div>
+        <body>
+          <div id="contenedor">
+            <div id="contenidos">
+              <div id="columna1">
+                {/* Barra lateral */}
+                <Sidebar/>
+              </div>
+              <div id="columna2">
+                {/* Título "Gestión de Inventario de Producto Terminado" */}
+                <h1 align="center">Gestión de Materias Primas</h1>
+                <section className="menu-center">
+                <h3>Tabla de Materias Primas</h3>
+                <div className='App'>
+                  <BrowserRouter>
+                    <Routes>
+                    <Route path='/' element={ <ShowMateriasPrimas/>}/>
+                    <Route path='/create' element={ <CreateMateriasPrimas/>}/>
+                    <Route path='/edit/:id' element={ <EditMateriasPrimas/>}/>    
+                    </Routes>
+                  </BrowserRouter>
+                </div>
+                </section>
+                {/* Botón "Atrás" */}
+                <button className="back-button"> Atrás</button>
+              </div>
+            </div>
+          </div>
+        </body>           
       </div>
-  );
+    </div>
+  )
 }
-
 export default App;
+
---
+++ reactfront/src/componentes/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Navbar.css';
+//import { FaSearch } from 'react-icons/fa';
+
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="navbar-container">
+        {/* Logo */}
+        <div className="logo">
+          <img src="https://www.torogoz.com/wp-content/uploads/2017/05/torogoz_Mockup-10.png" alt="Logo del proyecto" width="238" height="72" className="fusion-mobile-logo" />          
+        </div>
+
+        {/* Mensaje de bienvenida */}
+        
+
+        {/* Barra de búsqueda */}
+        <div className="search-bar">
+          <div className="search-input-container">
+            <input type="text" placeholder="Buscar"/>
+          </div>
+          <button>Buscar</button>
+        </div>
+        <div className="welcome-message">¡Bienvenido <a>Usuario!</a></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
